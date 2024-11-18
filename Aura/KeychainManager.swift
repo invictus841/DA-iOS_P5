9d60337e325bb852638cf146651f2eb4b5416299
invictus841 <usedforfunny@gmail.com>
2024-11-18 17:14:22 +0100

    Added a KeychainManager for token.

---
+++ Aura/KeychainManager.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainManager.swift
+//  Aura
+//
+//  Created by Alexandre Talatinian on 18/11/2024.
+//
+
+import Foundation
---
+++ Aura/Managers/KeychainManager.swift
@@ -0,0 +1,63 @@
+//
+//  KeychainManager.swift
+//  Aura
+//
+//  Created by Alexandre Talatinian on 18/11/2024.
+//
+
+import Foundation
+import Security
+
+class KeychainManager {
+    static let standard = KeychainManager()
+    private init() {} // Singleton
+    
+    let account = "com.mobilemaker.Aura"  // Change this to your app's bundle ID
+    
+    func save(_ token: String) -> Bool {
+        let tokenData = token.data(using: .utf8)!
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: tokenData
+        ]
+        
+        // First remove any existing token
+        SecItemDelete(query as CFDictionary)
+        
+        // Then save the new token
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        print("token saved")
+        return status == errSecSuccess
+    }
+    
+    func getToken() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        guard status == errSecSuccess,
+              let tokenData = result as? Data,
+              let token = String(data: tokenData, encoding: .utf8) else {
+            return nil
+        }
+        
+        return token
+    }
+    
+    func deleteToken() {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+    }
+}
