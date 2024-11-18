d2351aee1e70a9b8d504b7a0a48115874e68e3e1
invictus841 <usedforfunny@gmail.com>
2024-11-18 05:26:18 +0100

    Test Change.

---
+++ Aura/ViewModels/TransactionListViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  TransactionListViewModel.swift
+//  Aura
+//
+//  Created by Alexandre Talatinian on 9/11/2024.
+//
+
+import Foundation
+
+class TransactionListViewModel: ObservableObject {
+    @Published var transactions: [Transaction] = []
+    
+    init(accountDetailViewModel: AccountDetailViewModel) {
+//        self.transactions = accountDetailViewModel.getAllTransactions()
+        
+        print("All transactions:")
+        self.transactions.forEach { transaction in
+            print("\(transaction.description): \(transaction.amount)")
+        }
+    }
+}
