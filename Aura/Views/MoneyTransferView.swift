c921ad23203696b51b8b0d7326541d1043ba912d
invictus841 <usedforfunny@gmail.com>
2024-11-24 01:57:01 +0100

    MoneyTransferView has now validation. Alert will be shown when successfully money sent or failed or textfields not valid.

--- Aura/Views/MoneyTransferView.swift
+++ Aura/Views/MoneyTransferView.swift
@@ -9,75 +9,107 @@ import SwiftUI
 
 struct MoneyTransferView: View {
     @ObservedObject var viewModel = MoneyTransferViewModel()
-
-        @State private var animationScale: CGFloat = 1.0
-
-        var body: some View {
-            VStack(spacing: 20) {
-                // Adding a fun header image
-                Image(systemName: "arrow.right.arrow.left.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80)
-                    .foregroundColor(Color(hex: "#94A684"))
-                    .padding()
-                    .scaleEffect(animationScale)
-                    .onAppear {
-                        withAnimation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
-                            animationScale = 1.2
-                        }
+    @State private var animationScale: CGFloat = 1.0
+    
+    @State private var showAlert = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "arrow.right.arrow.left.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .foregroundColor(Color(hex: "#94A684"))
+                .padding()
+                .scaleEffect(animationScale)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
+                        animationScale = 1.2
                     }
-                
-                Text("Send Money!")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-
-                VStack(alignment: .leading) {
-                    Text("Recipient (Email or Phone)")
-                        .font(.headline)
-                    TextField("Enter recipient's info", text: $viewModel.recipient)
-                        .padding()
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(8)
-                        .keyboardType(.emailAddress)
                 }
-                
-                VStack(alignment: .leading) {
-                    Text("Amount (€)")
-                        .font(.headline)
-                    TextField("0.00", text: $viewModel.amount)
-                        .padding()
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(8)
-                        .keyboardType(.decimalPad)
-                }
-
-                Button(action: viewModel.sendMoney) {
-                    HStack {
-                        Image(systemName: "arrow.right.circle.fill")
-                        Text("Send")
-                    }
+            
+            Text("Send Money!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            
+            VStack(alignment: .leading) {
+                Text("Recipient (Email or Phone)")
+                    .font(.headline)
+                TextField("Enter recipient's info", text: $viewModel.recipient)
                     .padding()
-                    .background(Color(hex: "#94A684"))
-                    .foregroundColor(.white)
+                    .background(Color.gray.opacity(0.2))
                     .cornerRadius(8)
-                }
-                .buttonStyle(PlainButtonStyle())
-
-                // Message
-                if !viewModel.transferMessage.isEmpty {
-                    Text(viewModel.transferMessage)
-                        .padding(.top, 20)
-                        .transition(.move(edge: .top))
-                }
-                
-                Spacer()
+                    .keyboardType(.emailAddress)
             }
-            .padding()
-            .onTapGesture {
-                        self.endEditing(true)  // This will dismiss the keyboard when tapping outside
-                    }
+            
+            VStack(alignment: .leading) {
+                Text("Amount (€)")
+                    .font(.headline)
+                TextField("0.00", text: $viewModel.amount)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Button(action: validateAndSend) {
+                HStack {
+                    Image(systemName: "arrow.right.circle.fill")
+                    Text("Send")
+                }
+                .padding()
+                .background(Color(hex: "#94A684"))
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            }
+            
+            
+            if !viewModel.transferMessage.isEmpty && !viewModel.showError {
+                Text(viewModel.transferMessage)
+                    .foregroundColor(.green)
+                    .padding(.top, 20)
+            }
+            
+            Spacer()
         }
+        .padding()
+        .onTapGesture {
+            self.endEditing(true)
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Error"),
+                message: Text(viewModel.transferMessage),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+    }
+    
+    private func validateAndSend() {
+        if viewModel.recipient.isEmpty {
+            viewModel.transferMessage = "Please enter recipient"
+            viewModel.showError = true
+            showAlert = true
+            return
+        }
+        
+        let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}+"
+            if !NSPredicate(format:"SELF MATCHES %@", emailPattern).evaluate(with: viewModel.recipient) {
+                viewModel.transferMessage = "Please enter a valid email address"
+                viewModel.showError = true
+                showAlert = true
+                return
+            }
+        
+        if viewModel.amount.isEmpty {
+            viewModel.transferMessage = "Please enter amount"
+            viewModel.showError = true
+            showAlert = true
+            return
+        }
+        
+        viewModel.sendMoney()
+    }
 }
 
 
